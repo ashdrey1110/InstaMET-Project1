acd9645b07093efd6781c51fde64c176c35bb81a
Ashley Reynolds <ashrey1110@Ashleys-MacBook-Pro.local>
2025-02-14 16:49:18 -0800

    fetch might be asking for too much data

--- scripts/METgetter.js
+++ scripts/METgetter.js
@@ -1,5 +1,60 @@
 // METgetter.js captures 10 random objectIDs and their respective key-values
 
-function artSelector {
+// Create a new array of objects, which will hold 10 random pieces
+let arrayOfTenPieces = [];
+
+function artSelector() {
    // Randomly select an index between 0 and 494093 
+   let randomIndex = Math.round(Math.random() * (494093 - 0) + 0);
+   console.log(`the random index is ${randomIndex}`);
+
+   let objectID;
+
+   // Fetch & jsonify 
+   fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects`)
+    .then(rawData => {
+        if(!rawData.ok){
+            throw new Error(`code: ${rawData.status}, status text: ${rawData.statusText}`);
+        } 
+        else {return rawData.json();}
+    })
+    .then(jsonifiedData => {
+        objectID = jsonifiedData.objectIDs[randomIndex];
+        console.log(`the object id is ${objectID}`);
+    })
+    // Now, with the objectID, lets get some info on the art piece
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+            .then(rawArt => {
+                if(!rawArt.ok){
+                    throw new Error(`code: ${rawArt.status}, status text: ${rawArt.statusText}`);
+                } 
+                else {return rawArt.json();}
+            })
+            .then(jsonifiedArt => {
+                let artImage = jsonifiedArt.primaryImageSmall;
+                let artTitle = jsonifiedArt.title;
+                let artistName = jsonifiedArt.artistDisplayName;
+                let artistBio = jsonifiedArt.artistDisplayBio;
+                let artDate = jsonifiedArt.objectDate;
+                let artMedium = jsonifiedArt.medium;
+                let artWiki = jsonifiedArt.objectWikidata_URL;
+                let artDepartment = jsonifiedArt.department;
+                let artGalleryNum = jsonifiedArt.GalleryNumber;
+
+                arrayOfTenPieces.push({
+                    title: artTitle,
+                    fullName: artistName,
+                    image: artImage,
+                    bio: artistBio,
+                    date: artDate,
+                    medium: artMedium,
+                    wiki: artWiki,
+                    department: artDepartment,
+                    gallery: artGalleryNum
+                })
+            })  
+    // print the object
+    console.log(arrayOfTenPieces);
 }
+
+artSelector();
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+// Retrieve MET info
+//There are about 490k pieces, with objectIDs that do not necessarily match with the index.
+
+// Create a function that will generate 10 random pieces 
